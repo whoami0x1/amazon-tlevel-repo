1cbcc438196400f846704c2139089217a125400c
HeXif <100049779+xHeXifx@users.noreply.github.com>
2026-06-22 14:14:16 +0100

    Submit and store Expression of Interests to Dynamo

--- api-server/server.js
+++ api-server/server.js
@@ -3,18 +3,19 @@ const axios = require("axios");
 const qs = require("querystring");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const logger = require('./logger')
+const dynamo = require('./dynamo')
 
 require("dotenv").config(); 
 
 // S3 imports
-const { S3Client, ListObjectsV2Command, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, ListObjectsV2Command, GetObjectCommand, AnnotationDirective } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const app = express();
 const PORT = 5501;
 
 app.use(cookieParser());
+app.use(express.json())
 app.use(cors({
     origin: "http://localhost:5500",
     credentials: true
@@ -104,7 +105,9 @@ app.get("/api/cognitoCallback", async (req, res) => {
             secure: false,
             sameSite: "lax"
         });
-
+        const payload = jwt.decode(tokens.id_token)
+        username = payload["cognito:username"] || payload.username
+        dynamo.logData(req, res, `User ${username} logged in.`)
         res.redirect("http://localhost:5500/auth-success.html");
 
     } catch (err) {
@@ -129,7 +132,7 @@ app.get("/api/s3/getFiles", async (req, res) => {
                 });
 
                 const url = await getSignedUrl(s3, command, {
-                    expiresIn: 60 * 5 // 5 minutes
+                    expiresIn: 60 * 5
                 });
 
                 return {
@@ -141,7 +144,7 @@ app.get("/api/s3/getFiles", async (req, res) => {
             })
         );
 
-        logger.logData(req, res, "Requested all S3 files.")
+        dynamo.logData(req, res, "Requested all S3 files.")
         return res.json({ files });
 
     } catch (err) {
@@ -152,11 +155,36 @@ app.get("/api/s3/getFiles", async (req, res) => {
 
 app.get("/api/log", async (req, res) => {
     try {
-        logger.logData(req, res, req.query.data)
+        dynamo.logData(req, res, req.query.data)
     } catch (err) {
         console.log(err)
         return res.status(500).json({ error: "Failed to log" })
     }
+});
+
+app.post("/api/eoi/submit", async (req, res) => { // route name is self explainitory
+    try {
+        const data = req.body;
+        console.log(data)
+        const eoiRes = await dynamo.submitEOI(req, res, data);
+
+        if (eoiRes.error) {
+            res.status(500).json({ error: `Failed to submit EOI: ${eoiRes.error}`});
+        } else {
+            return { success: true };
+        }
+    } catch (err) {
+        console.log(`Failed to submit EOI: ${err}`);
+        res.status(500).json({ error: "Failed to submit EOI" });
+    }
+});
+
+app.get('/api/eoi/list', async (req, res) => { // list all EOIs
+
+});
+
+app.get('/api/eoi/view', async (req, res) => { // Read a specific one (list should just return like course and title)
+
 });
 
 app.listen(PORT, () => {
